5182b06fa984be4b4d2d41b9616d4ad1601c3e1a
chaimaboutou <chaima.boutou@gmail.com>
2024-06-06 07:58:27 +0100

    update

--- src/Component/OrderDetails.js
+++ src/Component/OrderDetails.js
@@ -11,15 +11,15 @@ const OrderDetails = () => {
     const itemsJson = query.get('items');
 
     const [dateTime, setDateTime] = useState('');
+    const [requestSent, setRequestSent] = useState(false);
 
     useEffect(() => {
-        if (itemsJson) {
+        if (itemsJson && !requestSent) {
             const items = JSON.parse(decodeURIComponent(itemsJson));
 
             const now = new Date();
             const formattedDateTime = `${now.toLocaleDateString()} ${now.toLocaleTimeString()}`;
             setDateTime(formattedDateTime);
-
 
             axios.post('http://localhost:9001/api/order', items.items, {
                 headers: {
@@ -29,14 +29,15 @@ const OrderDetails = () => {
                 .then(response => {
                     setOrderResponse(response.data);
                     setLoading(false);
-                    console.log(orderResponse)
+                    setRequestSent(true);
                 })
                 .catch(error => {
                     console.error('Error fetching order details:', error);
                     setLoading(false);
+                    setRequestSent(false);
                 });
         }
-    }, [itemsJson]);
+    }, [itemsJson, requestSent]);
 
     if (loading) {
         return <p>Loading...</p>;
@@ -53,20 +54,20 @@ const OrderDetails = () => {
                             <span>{dateTime}</span>
                         </div>
                         <div className="bill-item">
-                            <span>Total Cost of the oreder:</span>
-                            <span>{orderResponse.totalCost}</span>
+                            <span>Total Cost of the order:</span>
+                            <span>{parseFloat(orderResponse.totalCost).toFixed(2)}</span>
                         </div>
                     </div>
                     <div className="bill-header">Items</div>
                     {orderResponse.orderItems.map((item, index) => (
                         <div key={index} className="bill-item">
                             <span>{item.itemName}</span>
-                            <span>Price: {item.price}</span>
+                            <span>Price: {parseFloat(item.price).toFixed(2)}</span>
                             <span>Quantity: {item.quantity}</span>
                         </div>
                     ))}
                     <div className="bill-footer">
-                        <span>Total:</span> {orderResponse.totalCost}
+                        <span>Total:</span> {parseFloat(orderResponse.totalCost).toFixed(2)}
                     </div>
                 </>
             ) : (
